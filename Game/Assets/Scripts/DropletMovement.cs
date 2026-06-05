6a9a61bc25fe36166109f8c5ce0ee5a364c4407a
cemt1363 <cem.temizkan@gmail.com>
2026-06-05 23:33:17 +0200

    messing around with the droplet

--- Game/Assets/Scripts/DropletMovement.cs
+++ Game/Assets/Scripts/DropletMovement.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class DropletMovement : MonoBehaviour
 {
+
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = movement.normalized * moveSpeed;
     }
 }
