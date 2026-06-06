b7b188ffa93648635590c053bcb75fca29d07715
cemt1363 <cem.temizkan@gmail.com>
2026-06-06 17:00:28 +0200

    levels ig

--- Game/Assets/Scripts/PatternSelecter.cs
+++ Game/Assets/Scripts/PatternSelecter.cs
@@ -2,13 +2,24 @@ using UnityEngine;
 
 public class PatternSelecter : MonoBehaviour
 {
+
+
+    bool isStarting = true;
     void Start()
     {
-        foreach (Transform child in transform)
-            child.gameObject.SetActive(false);
+        
+        if (isStarting)
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+        } else
+        {
+            foreach (Transform child in transform)
+                child.gameObject.SetActive(false);
 
-        int randomIndex = Random.Range(0, transform.childCount);
+            int randomIndex = Random.Range(1, transform.childCount);
 
-        transform.GetChild(randomIndex).gameObject.SetActive(true);
+            transform.GetChild(randomIndex).gameObject.SetActive(true);
+        }
+
     }
 }
