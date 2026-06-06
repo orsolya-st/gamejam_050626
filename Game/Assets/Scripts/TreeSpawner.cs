af293c1a6288a66a657a14eb19526857bc928093
cemt1363 <cem.temizkan@gmail.com>
2026-06-06 13:26:01 +0200

    tree sprite added and trying to debug the tree cloning

--- Game/Assets/Scripts/TreeSpawner.cs
+++ Game/Assets/Scripts/TreeSpawner.cs
@@ -5,21 +5,10 @@ public class TreeSpawner : MonoBehaviour
     public GameObject treePrefab;
     public Transform spawnPoint;
 
-    private bool hasSpawned = false;
-    private static float nextAllowedSpawnTime = 0f;
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Player")) return;
-        if (hasSpawned) return;
-
-        if (Time.time < nextAllowedSpawnTime) return;
-
-        hasSpawned = true;
-        nextAllowedSpawnTime = Time.time + 1f;
 
         Instantiate(treePrefab, spawnPoint.position, Quaternion.identity);
-
-        gameObject.SetActive(false);
     }
 }
