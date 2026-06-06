48c5da2894128155245e98621a2644b66a3bfeb3
cemt1363 <cem.temizkan@gmail.com>
2026-06-06 14:45:55 +0200

    no more infinite trees

--- Game/Assets/Scripts/TreeSpawner.cs
+++ Game/Assets/Scripts/TreeSpawner.cs
@@ -9,19 +9,23 @@ public class TreeSpawner : MonoBehaviour
 
     void Update()
     {
-
-        Debug.Log("TreeSpawner running");
-
-        if (hasSpawned) return;
-
-        if (player.position.y < transform.position.y - 3f)
+        if (hasSpawned == false)
         {
-            hasSpawned = true;
+            if (player.position.y < transform.position.y - 3f)
+            {
+                hasSpawned = true;
 
-            float treeHeight = GetComponent<SpriteRenderer>().bounds.size.y;
-            Vector3 spawnPos = (transform.position + Vector3.down * treeHeight) - new Vector3(0,-0.2f,0);
+                float treeHeight = GetComponent<SpriteRenderer>().bounds.size.y;
+                Vector3 spawnPos = transform.position + Vector3.down * treeHeight + new Vector3(0, 0.2f, 0);
 
-            Instantiate(treePrefab, spawnPos, Quaternion.identity);
+                Instantiate(treePrefab, spawnPos, Quaternion.identity);
+            }
+        }
+
+        // Delete trees that are far above the player
+        if (player.position.y < transform.position.y - 40f)
+        {
+            Destroy(gameObject);
         }
     }
 }
