7070d4506fdb1a4796ace5d9f0d50429e2da35a3
orsolya-st <steinmetz.orsi@gmail.com>
2026-06-06 17:05:11 +0200

    md

--- Game/Assets/Scripts/MiniDroplet.cs
+++ Game/Assets/Scripts/MiniDroplet.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class MiniDroplet : MonoBehaviour
 {
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        float size = Random.Range(0.3f, 0.7f);
+        Vector2 targetSize = new Vector2(size, size);
+
+        transform.localScale = targetSize;
     }
 
     // Update is called once per frame
