63c8ac4ec09b5fd0386592e0408bbcf9e9a60b56
go38coz <go38coz@mytum.de>
2026-06-07 07:54:04 +0200

    Made winning reset tree counter; Dry leaf duration 2 -> 1

--- Game/Assets/Scripts/DamageHandler.cs
+++ Game/Assets/Scripts/DamageHandler.cs
@@ -75,11 +75,12 @@ public class DamageHandler : MonoBehaviour
 	}
 
 	private void OnCollisionEnter2D(Collision2D other)
-{
-		if(other.collider.CompareTag("Finish")){
+	{
+		if(other.collider.CompareTag("Finish")){ //WIN THE GAME
+			TreeSpawner.treeCount = 0;
 			SceneManager.LoadScene(5, LoadSceneMode.Single);
 		}
-    float fallSpeed = lastYVelocity;        
+		float fallSpeed = lastYVelocity;        
     
     // Check if we hit hard enough
     if (fallSpeed < -threshold)
--- Game/Assets/Scripts/PatternSelecter.cs
+++ Game/Assets/Scripts/PatternSelecter.cs
@@ -7,8 +7,8 @@ public class PatternSelecter : MonoBehaviour
     public int lastTreeNumber = 8;
     public SpriteRenderer spriteRenderer;
     public Sprite endTreeSprite;
-    public BoxCollider2D confine;
-    public CinemachineConfiner2D confiner2D;
+    private BoxCollider2D confine;
+    private CinemachineConfiner2D confiner2D;
 
     void Start()
     {
@@ -30,6 +30,7 @@ public class PatternSelecter : MonoBehaviour
         {
             transform.GetChild(transform.childCount - 1).gameObject.SetActive(true);
             spriteRenderer.sprite = endTreeSprite;
+            // spriteRenderer.color = new Color32(255, 228, 225,255);
             transform.localScale = new Vector3(1.05f, 1.05f, 1.05f);
             transform.localPosition += new Vector3(0.27f,0.54f,0f);
             confine.size += new Vector2(0f, 38f);
